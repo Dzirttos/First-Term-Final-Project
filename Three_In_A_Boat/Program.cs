cfba7ac601e705f1624b8132a07a6639b8acc4e2
Dizrtt <demonic2003@bk.ru>
2022-12-04 12:48:58 +0200

    'FillArray' method created.

--- Three_In_A_Boat/Program.cs
+++ Three_In_A_Boat/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символам. 
+Console.Clear();
+
+
+
+string[] FillArray(int size)
+{
+    string[] userArray = new string[size];
+    for (int j = 0; j < userArray.Length; j++)
+    {
+        Console.WriteLine($"Please enter array element {j + 1}:");
+        userArray[j] = (Console.ReadLine());
+    }
+    return userArray;
+}
